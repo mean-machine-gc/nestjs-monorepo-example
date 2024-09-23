2ccd1e5acb3a71db4a0e28484430ea63aabd280a
Giovanni Chiodi <chiodi@unicc.org>
2024-09-23 12:59:40 +0200

    add repository methods

--- apps/workflows-service/src/workflows/workflows.module.ts
+++ apps/workflows-service/src/workflows/workflows.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { WorkflowsService } from './workflows.service';
 import { WorkflowsController } from './workflows.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Workflow } from './entities/workflow.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Workflow])],
   controllers: [WorkflowsController],
   providers: [WorkflowsService],
 })
