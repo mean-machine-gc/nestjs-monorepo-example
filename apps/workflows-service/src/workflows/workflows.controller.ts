d2ee067423409a10a2e75350dc4b2dce840959ef
Giovanni Chiodi <chiodi@unicc.org>
2024-09-23 13:10:21 +0200

    add workflow library

--- apps/workflows-service/src/workflows/workflows.controller.ts
+++ apps/workflows-service/src/workflows/workflows.controller.ts
@@ -1,7 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { WorkflowsService } from './workflows.service';
-import { CreateWorkflowDto } from './dto/create-workflow.dto';
-import { UpdateWorkflowDto } from './dto/update-workflow.dto';
+import { CreateWorkflowDto, UpdateWorkflowDto } from '@app/workflows';
 
 @Controller('workflows')
 export class WorkflowsController {
